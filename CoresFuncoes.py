af7e38e63da6f208621be33aa874852666a34163
Wollacy Willyan de Carvalho <carvalho_1990@outlook.com>
2021-01-04 20:07:32 -0300

    Create CoresFuncoes.py

---
+++ CoresFuncoes.py
@@ -0,0 +1,14 @@
+## Adicionando Cores
+##\033[formatação:corTexto:corFundom
+
+print('\033[0;30;41mOlá mundo!')
+print('\033[4;33;44mOlá mundo!')
+print('\033[1;35;43mOlá mundo!')
+print('\033[30;42mOlá mundo!\033[0;0;0m')
+print('\033[mOlá mundo!')
+print('\033[7;33;44mOlá mundo!')
+
+## Verifica quantidade de caracteres em uma string
+
+s='Prova de Python'
+print(len(s))
