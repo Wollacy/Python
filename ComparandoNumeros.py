f96e75f99a80428540a894da1f40813b264a4803
Wollacy Willyan de Carvalho <carvalho_1990@outlook.com>
2021-01-05 18:10:55 -0300

    Create ComparandoNumeros.py

---
+++ ComparandoNumeros.py
@@ -0,0 +1,11 @@
+## Comparando Números
+
+n1=int(input('Digite o primeiro valor: '))
+n2=int(input('Digite o segundo númer: '))
+
+if n1>n2:
+  print('O Primeiro valor é maior!')
+elif n2>n1:
+  print('O segundo número é maior!')
+else:
+  print('Os dois números são iguais!')
