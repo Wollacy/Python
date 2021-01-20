a5bfefbd913eb66ab74079a592e82877ff285188
Wollacy <carvalho_1990@outlook.com>
2021-01-20 17:19:10 -0300

    Create FormatacaoTexto.py

---
+++ FormatacaoTexto.py
@@ -0,0 +1,8 @@
+## Formatação de Texto
+
+nome=input('Digite seu m=nome completo: ')
+print(nome.upper())
+print(nome.lower())
+nome2=nome.strip()
+print(len(nome2)-nome2.count(' '))
+print(nome2.find(' '))
