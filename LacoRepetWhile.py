55f436a3150373fed9083a9928a804038e28a5d9
Wollacy <carvalho_1990@outlook.com>
2021-01-21 08:41:14 -0300

    Create LacoRepetWhile.py

---
+++ LacoRepetWhile.py
@@ -0,0 +1,18 @@
+## Laço de Repetição While
+
+c = 1
+while c < 10:
+  print(c)
+  c +=1
+print('Fim')
+
+n = 1
+while n != 0:
+  n=int(input('Digite um valor: '))
+print('Fim!')
+
+r = 'S'
+while r == 'S':
+  n=int(input('Digite um valor: '))
+  r=str(input('Quer continuar? (S/N)')).upper()
+print('Fim!')
