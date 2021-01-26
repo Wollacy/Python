30c65942cfa5e85df4381f9e0c8a89d2ee0e6394
Wollacy <carvalho_1990@outlook.com>
2021-01-26 13:59:22 -0300

    Create Listas.py

---
+++ Listas.py
@@ -0,0 +1,20 @@
+## Listas
+
+lista1 = [1,2,3,4,5]
+lista2 = ["Olá","Mundo","!"]
+lista3 = [0, "Olá", "Biscoito", "Bolacha", 9.99, True]
+
+print('')
+
+for i in lista1:
+  print(i)
+
+print('')
+
+for i in lista2:
+  print(i)
+
+print('')
+
+for i in lista3:
+  print(i)
