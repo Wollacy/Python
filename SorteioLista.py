5ffe1d58779b8bb98af423c625a56b63800f896c
Wollacy <carvalho_1990@outlook.com>
2021-01-26 13:59:29 -0300

    Update SorteioLista.py

--- SorteioLista.py
+++ SorteioLista.py
@@ -3,9 +3,7 @@
 import random
 from time import sleep
 
-lista=['1-Livia','2-Jenifer','4-Juce','5-Michelli','7-Camila','8-Deisy','9-Jenifer','13-Cristiane','14-Amanda(Thamy)','15-Juce','16-Philipp','17-Du','18-Tânia(Mãe)',
-       '20-Liciane','21-Sarah','22-Juce','24-Josi','25-Leda','26-Thaynara','27-Juce','29-Iane','30-Laise','31-tia Cátia','35-Amanda','36-Isabel','37-Thamy',
-       '38-Camila(Jonathan)','39-Marli','42-Jane','43-Nana','44-Mara','45-Lizana','46-Dada','47-Cátia(Carlyles)','49-Benjamim','50-João']
+lista=['1-Livia','2-Jenifer','4-Juce','5-Michelli','7-Camila','8-Deisy','9-Jenifer','13-Cristiane','14-Amanda(Thamy)','15-Juce','16-Philipp','17-Du','18-Tânia(Mãe)','20-Liciane','21-Sarah','22-Juce','24-Josi','25-Leda','26-Thaynara','27-Juce','29-Iane','30-Laise','31-tia Cátia','35-Amanda','36-Isabel','37-Thamy','38-Camila(Jonathan)','39-Marli','42-Jane','43-Nana','44-Mara','45-Lizana','46-Dada','47-Cátia(Carlyles)','49-Benjamim','50-João']
 for i in lista:
   print(i)
 
