89f332e11dacb460f32e231098a930c457981f8e
Wollacy <carvalho_1990@outlook.com>
2021-01-27 12:35:22 -0300

    Update VerificarStr.py

--- VerificarStr.py
+++ VerificarStr.py
@@ -2,3 +2,21 @@
 
 nome = str(input('Qual seu nome completo? ')).strip()
 print('Seu nome tem Silva? {}'.format('SILVA' in nome.upper()))
+
+## Concatenando Strings
+
+print('-='*30)
+print('CONCATENANDO STRINGS')
+print('-='*30)
+
+a = 'Wollacy'
+b = 'Lilian'
+c = 'Augusto'
+
+print(a)
+print(b)
+print(c)
+
+d = a + ' + ' + b + ' = ' + c
+
+print(d)
