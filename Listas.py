fe440c203504e625456bb64c030baf34cd9e438a
Wollacy Willyan de Carvalho <carvalho_1990@outlook.com>
2021-03-18 21:11:02 -0300

    Update Listas.py

--- Listas.py
+++ Listas.py
@@ -4,6 +4,11 @@ lista1 = [1,2,3,4,5]
 lista2 = ["Olá","Mundo","!"]
 lista3 = [0, "Olá", "Biscoito", "Bolacha", 9.99, True]
 
+print(lista3)
+
+if "Mundo" in lista3:
+  lista3.pop()
+
 print('')
 
 for i in lista1:
