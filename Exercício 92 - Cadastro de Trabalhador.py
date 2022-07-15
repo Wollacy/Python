30e821fe350365d05d21f3c58f12d40a830289f0
Wollacy Willyan de Carvalho <carvalho_1990@outlook.com>
2022-07-15 06:34:12 -0300

    Update Exercício 92 - Cadastro de Trabalhador.py

--- Exercício 92 - Cadastro de Trabalhador.py
+++ Exercício 92 - Cadastro de Trabalhador.py
@@ -1,11 +1,13 @@
 import datetime
 
+cadGeral = list()
 cadastro = dict()
 
 dataAtual = datetime.datetime.now() 
 anoAtual = dataAtual.year
 
 while True:
+    cadastro.clear()
     cadastro['nome'] = str(input('Nome: '))
     anoNascimento = int(input('Ano do Nascimento: '))
     
@@ -17,11 +19,29 @@ while True:
     if cadastro['ctps'] != 0:
         cadastro['anoContratacao'] = int(input('Qual o ano da contratação: '))
         cadastro['salario'] = float(input('Salário: R$ '))
+        cadastro['aposentadoria'] = cadastro['idade'] + (cadastro['anoContratacao'] + 35) - anoAtual
 
-    resposta = str(input('Quer continuar? S/N')).strip().upper()[0]
+    cadGeral.append(cadastro.copy())
+
+    while True:
+        resposta = str(input('Quer continuar? S/N ')).strip().upper()[0]
+        if resposta in 'SN':
+            break
+        print('Resposta inválida. Selecione apesa "S" ou "N"!')
 
     if resposta == 'N':
         break
 
-for k, v in cadastro.items():
-    print(f' - {k} em {v}')
+print('*='*30)
+for p in cadGeral:
+    for k, v in p.items():
+        print(f'{k} = {v}; ', end='')
+    print()
+
+print('*='*30)
+for p in cadGeral:
+    if p['ctps'] > 0:
+        print(f'{p["nome"]} vai se aposentar com {p["aposentadoria"]} anos.')
+
+print('*='*30)
+print('>>>>>>>>>> FINALIZADO <<<<<<<<<<')
