6dc2fa06f2e5caf0afc5f5b2e692ced7929694f1
Wollacy Willyan de Carvalho <carvalho_1990@outlook.com>
2022-07-16 06:56:24 -0300

    Update Exercício 93 - Cadastro de Jogador.py

--- Exercício 93 - Cadastro de Jogador.py
+++ Exercício 93 - Cadastro de Jogador.py
@@ -1,28 +1,68 @@
+from ast import Break
+
+
 jogador = dict()
 partidas = list()
+cadastro = list()
 
-jogador['nome'] = str(input('Nome do jogador: '))
-tot = int(input(f'Quantas partidas o {jogador["nome"]} jogou? '))
+while True:
+    jogador.clear()
+    jogador['nome'] = str(input('Nome do jogador: '))
+    tot = int(input(f'Quantas partidas o {jogador["nome"]} jogou? '))
+
+    partidas.clear()
+
+    cont = 0
+    for c in range(0, tot):
+        cont += 1
+        partidas.append(int(input(f'   Quantos gols na partida {cont}? ')))
+
+    jogador['gols'] = partidas[:]
+    jogador['total'] = sum(partidas)
+    cadastro.append(jogador.copy())
+
+    while True:
+        resposta = str(input('Deseja continuar? S/N ')).strip().upper()[0]
+
+        if resposta in 'SN':
+            break
+
+        print('Resposta inválida. Digite "S" para "SIM" ou "N" para "NÃO"!')
+    
+    if resposta == 'N':
+            break
+
+print('-='*10, ' JOGADORES CADASTRADOS ', '-='*10)
+print(f"{'Cód.':<5}", end='')
+print(f"{'Nome':<20}", end='')
+print(f"{'Gols':<20}", end='')
+print(f"{'Total':<10}")
+print('-=' *40)
+print()
 
 cont = 0
-for c in range(0, tot):
+for k, v in enumerate(cadastro):
     cont += 1
-    partidas.append(int(input(f'   Quantos gols na partida {cont}? ')))
+    print(f'{cont:<5}', end='')
+    for d in v.values():
+        print(f'{str(d):<20}', end='')
+    print()
 
-jogador['gols'] = partidas[:]
-jogador['total'] = sum(partidas)
+while True:
+    print()
+    mostrar=int(input('Qual jogador você quer detalhar? (999 para sair)'))
+    print()
+    
+    if mostrar == 999:
+        break
+    
+    busca=mostrar-1
+    if busca >= len(cadastro):
+        print(f'ERRO! Não existe jogador com o código {mostrar}')
+    else:
+        print(f'Levantamento do jogador {cadastro[busca]["nome"]}:')
+        for i, g in enumerate(cadastro[busca]['gols']):
+            print(f'      No jogo {i+1} fez {g} gols.')
+        print('-'*30)
 
-print('-='*30)
-print(jogador)
-print('-='*30)
-
-for k, v in jogador.items():
-    print(f'O campo {k} tem o valor {v}!')
-
-print('-='*30)
-print(f'O jogador {jogador["nome"]} jogou {len(jogador["gols"])} partidas.')
-
-for i, v in enumerate(jogador['gols']):
-    print(f'     => Na partida {i+1}, fez {v} gols.')
-
-print(f'Foi um total de {jogador["total"]} gols')
+print('>>>>>>>> ATÉ MAIS! <<<<<<<<')
