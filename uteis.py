7495c4cc508aa9a1aa608c9fe2f6e07502846172
Wollacy Willyan de Carvalho <carvalho_1990@outlook.com>
2022-08-07 05:18:34 -0300

    Update uteis.py

--- uteis.py
+++ uteis.py
@@ -3,3 +3,21 @@ def fatorial(n):
     for c in range(1, n+1):
         f *= c
     return f
+
+def aumentar(n):
+    p = (n * 10) / 100
+    aumentar = p + n
+    return aumentar
+
+def diminuir(n):
+    p = (n * 13) / 100
+    diminuir = n - p
+    return diminuir
+
+def dobro(n):
+    dobro = n * 2
+    return dobro
+
+def metade(n):
+    metade = n / 2
+    return metade
