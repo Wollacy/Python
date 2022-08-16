e2850d750c59c01ee58de82aff6c1562a37dfb98
Wollacy Willyan de Carvalho <carvalho_1990@outlook.com>
2022-08-16 06:22:57 -0300

    Create moeda107.py

---
+++ mod EX 107/moeda107.py
@@ -0,0 +1,20 @@
+def aumentar(n):
+    p = (n * 10) / 100
+    aumentar = p + n
+    return aumentar
+
+
+def diminuir(n):
+    p = (n * 13) / 100
+    diminuir = n - p
+    return diminuir
+
+
+def dobro(n):
+    dobro = n * 2
+    return dobro
+
+
+def metade(n):
+    metade = n / 2
+    return metade
