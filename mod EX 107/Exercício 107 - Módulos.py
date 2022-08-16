6ba46930e5a7e84e11455b01909e8c1593a5b7e1
Wollacy Willyan de Carvalho <carvalho_1990@outlook.com>
2022-08-16 06:22:39 -0300

    Update Exercício 107 - Módulos.py

--- mod EX 107/Exercício 107 - Módulos.py
+++ mod EX 107/Exercício 107 - Módulos.py
@@ -1,10 +1,10 @@
-import moeda
+import moeda107
 
 n = float(input('Digite um preço: R$'))
-metade = moeda.metade(n)
-dobro = moeda.dobro(n)
-aumentar = moeda.aumentar(n)
-diminuir = moeda.diminuir(n)
+metade = moeda107.metade(n)
+dobro = moeda107.dobro(n)
+aumentar = moeda107.aumentar(n)
+diminuir = moeda107.diminuir(n)
 
 print(f'A metade de R${n:.2f} é R${metade:.2f}.')
 print(f'O dobro de R${n:.2f} é R${dobro:.2f}.')
