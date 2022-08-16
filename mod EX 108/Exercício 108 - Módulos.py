9548a94cab9d62b13660b969f7905ff578a02147
Wollacy Willyan de Carvalho <carvalho_1990@outlook.com>
2022-08-16 06:23:13 -0300

    Update Exercício 108 - Módulos.py

--- mod EX 108/Exercício 108 - Módulos.py
+++ mod EX 108/Exercício 108 - Módulos.py
@@ -1,8 +1,8 @@
-import moeda
+import moeda108
 
 n = float(input('Digite um preço: R$'))
 
-print(f'A metade de {moeda.moeda(n)} é {moeda.moeda(moeda.metade(n))}.')
-print(f'O dobro de {moeda.moeda(n)} é {moeda.moeda(moeda.dobro(n))}.')
-print(f'Aumentando 10%, temos {moeda.moeda(moeda.aumentar(n))}.')
-print(f'Diminuindo 13%, temos {moeda.moeda(moeda.diminuir(n))}.')
+print(f'A metade de {moeda108.moeda(n)} é {moeda108.moeda(moeda108.metade(n))}.')
+print(f'O dobro de {moeda108.moeda(n)} é {moeda108.moeda(moeda108.dobro(n))}.')
+print(f'Aumentando 10%, temos {moeda108.moeda(moeda108.aumentar(n))}.')
+print(f'Diminuindo 13%, temos {moeda108.moeda(moeda108.diminuir(n))}.')
